a1768b927af7adcacb94b5148acc21d8212595c8
Raphael Gottardo <raph@stat.ubc.ca>
2010-04-30 21:04:01 +0000

    # Change the name of the list when splitting for parallel computation
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PICS@46652 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/PICS.R
+++ R/PICS.R
@@ -25,6 +25,7 @@ PICS<-function(segReadsList,dataType="TF")
     nClust<-sfCpus()
     # Split into nClust segReadsList
     segSplit<-split(segReadsList,cut(1:length(segReadsList),nClust))
+    name(segSlit)<-NULL
     # Use a parallel version
     res<-unlist(sfLapply(segSplit,.fitModelAllkSplit,paraEM,paraPrior,minReads),recursive=FALSE)
   }
